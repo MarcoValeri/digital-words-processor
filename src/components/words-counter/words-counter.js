f53d37f351bb6289f4494cf8391c6f5e392b7619
Marco Valeri <marcovaleri@hotmail.it>
2022-07-17 14:15:38 +0100

    Style outputs

--- src/components/words-counter/words-counter.js
+++ src/components/words-counter/words-counter.js
@@ -19,14 +19,18 @@ const WordsCounter = () => {
     return (
         <div className='words-counter'>
             <div className="words-counter__grid">
+                <div className="words-counter__output-container">
+                    <div className="words-counter__output">
+                        <h4 className="words-counter__output-title">Words number</h4>
+                        <p className="words-counter__output-num">{wordsNum ? wordsNum.length : 0}</p>
+                    </div>
+                    <div className="words-counter__output">
+                        <h4 className="words-counter__output-title">Characters</h4>
+                        <p className="words-counter__output-num">{words.length}</p>
+                    </div>
+                </div>
                 <div className="words-counter__textarea-container">
                     <textarea className="words-counter__textarea" placeholder="Type or paste your text" onKeyUp={handleWords}></textarea>
-                </div>
-                <div className="words-counter__output-container">
-                    <ul>
-                        <li className="words-counter__output">Words number: {wordsNum ? wordsNum.length : 0}</li>
-                        <li className="words-counter__output">Characters: {words.length}</li>
-                    </ul>
                 </div>
             </div>
         </div>
