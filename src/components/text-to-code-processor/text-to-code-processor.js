0204bf2be79c3d14b22888d9f09ae7de8976426f
Marco Valeri <marcovaleri@hotmail.it>
2022-07-24 19:09:28 +0100

    Create text to code component

---
+++ src/components/text-to-code-processor/text-to-code-processor.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+// Import style file
+import './text-to-code-processor.scss';
+
+const TextToCodeProcessor = () => {
+
+    // Set Hooks
+    const [ words, setWords ] = useState('');
+
+    // Event
+    const handleWords = event => {
+
+        let getWords = event.target.value;
+        let flag = '';
+
+        for (let x = 0; x < getWords.length; x++) {
+            if (getWords[x] === "\n") {
+                flag = "true";
+            } else {
+                flag = "false";
+            }
+        }
+
+        setWords(flag);
+    }
+
+    return (
+        <div className='text-to-code-processor'>
+            <div className="text-to-code-processor__grid">
+                <div className="text-to-code-processor__textarea-container">
+                    <textarea className="text-to-code-processor__textarea" placeholder="Type or paste your text" onKeyUp={handleWords}></textarea>
+                </div>
+                <div className="text-to-code-processor__output-container">
+                    <p>output:</p>
+                    <p>{ words }</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TextToCodeProcessor;
--- src/pages/text-to-code/text-to-code.js
+++ src/pages/text-to-code/text-to-code.js
@@ -1,10 +1,15 @@
+// Import components
+import TextToCodeProcessor from '../../components/text-to-code-processor/text-to-code-processor';
+import Title from '../../components/title/title';
+
 // Import style file
 import './text-to-code.scss';
 
 const TextToCode = () => {
     return (
         <div>
-            <h2>Text To Code</h2>
+            <Title />
+            <TextToCodeProcessor />
         </div>
     )
 }
